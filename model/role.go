3892e9e0c091c46dfd0fe91bfd75c1953d63fe42
Miguel Soto <miguelsr.1084@gmail.com>
2022-09-27 21:31:34 -0500

    mapeo de modelos terminado

--- model/role.go
+++ model/role.go
@@ -5,8 +5,7 @@ import "time"
 type Roles struct {
 	ID        uint16    `json:"id"`
 	Name      string    `json:"name"`
-	IsActive  bool      `json:""`
-	BadgeType Badge     `json:"badge_type"`
+	IsActive  bool      `json:"is_active"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt time.Time `json:"deleted_at"`
